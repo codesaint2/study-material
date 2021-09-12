5ad8682e980a236e9489029a8fb900a77130fb5e
Code Saint <codesaint2@gmail.com>
2021-09-12 14:29:01 +0530

    added the problem statement for square in square

--- square-in-square/square-in-square.c
+++ square-in-square/square-in-square.c
@@ -1,3 +1,32 @@
+/*****************************************************************************************
+Problem : Square in a Square
+
+Idea is to take a number as input and print a pattern of boxes If input is 2, 
+two boxes are to be printed - one inside the other Smallest box will be of size 3x3, 
+the next bigger box will be 5x5, the next one will be 7x7, so on and so forth For input 1,
+then draw a box of dimensions 3x3 For input 2, outer box will be 5x5, 
+inner will be 3x3 For input 3, outer box will be 7x7, with 2 more inner boxes So for n, 
+outermost box will be nx2 +1 in size, with (n-1) inner boxes All boxes will be 
+top left aligned as shown in the figure.
+
+Input Format: First line of input contains a number N
+
+Output Format: Print N nested boxes
+
+Constraints: 0 < N < 25
+
+Sample Input: 2
+Sample Output:
+*****
+* * * 
+*** *
+*   *
+*****
+
+Time Limit: 5
+Memory Limit: 256
+*****************************************************************************************/
+
 #include <stdio.h>
 
 int main(){
